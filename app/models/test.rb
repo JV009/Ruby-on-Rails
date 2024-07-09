ceb58f100c69452e49bb8763c5f0e445519b223a
JV009 <krysov_aleksandr@mail.ru>
2024-07-09 12:31:18 +0300

    Added associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  has_many :questions
+  has_many :user_tests
+  has_many :users, through: :user_tests
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :category
   def self.tests_category(category_name)
     Test.joins('left join categories on categories.id = tests.category_id').where(categories: {title: category_name}).order(id: :desc).pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_tests
+  has_many :author_tests, class_name: 'Test', foreign_key: :author_id
+  has_many :tests, through: :user_tests
   def tests_level(level)
     Test.joins('left join user_tests on user_tests.test_id = tests.id').where(level: level, user_tests: { user_id: self.id })
   end
