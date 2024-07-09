50cb98380ce29557dd419111d2e8c4fdfb556342
JV009 <krysov_aleksandr@mail.ru>
2024-07-09 21:12:39 +0300

    Changed methods in user and test models

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,6 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :category
   def self.tests_category(category_name)
-    Test.joins('left join categories on categories.id = tests.category_id').where(categories: {title: category_name}).order(id: :desc).pluck(:title)
+    Test.where(categories: {title: category_name}).order(id: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
   has_many :tests, through: :user_tests
   def tests_level(level)
-    Test.joins('left join user_tests on user_tests.test_id = tests.id').where(level: level, user_tests: { user_id: self.id })
+    Test.where(level: level)
   end
 end
