1767ccebb0d2edf84688294a4c88568ed37988d2
JV009 <krysov_aleksandr@mail.ru>
2024-08-29 09:38:39 +0300

    Fixed method of creating

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -18,7 +18,7 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = @test.questions.create(question_params)
+    @question = @test.questions.new(question_params)
     if @question.save
       render plain: 'The question created!'
     else
