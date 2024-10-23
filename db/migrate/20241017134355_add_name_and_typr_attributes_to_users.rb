2b7afc25a6606187ad8fe49b4b23287de02f3952
JV009 <krysov_aleksandr@mail.ru>
2024-10-23 10:47:14 +0300

    Fix #3

--- db/migrate/20241017134355_add_name_and_typr_attributes_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddNameAndTyprAttributesToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :type, :string, null: false, default: 'User'
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_index :users, :type
-    remove_column :users, :name, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_17_134355) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_16_142630) do
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.boolean "correct", default: false, null: false
